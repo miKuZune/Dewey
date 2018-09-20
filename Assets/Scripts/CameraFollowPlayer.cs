4b94b264ff3d54889370c1d919e9aee2752d8a64
miKuZune <maxwfarley@live.co.uk>
2018-09-20 13:23:48 +0100

    Movement and beginnings of combat.

---
+++ Assets/Scripts/CameraFollowPlayer.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class CameraFollowPlayer : MonoBehaviour {
+
+    public GameObject player;
+
+    public float offsetX;
+    public float offsetY;
+
+    Vector3 offset;
+    // Use this for initialization
+    void Start ()
+    {
+		if(player == null){player = GameObject.FindGameObjectWithTag("Player");}
+
+        offset = Vector3.zero;
+        if (offsetX == 0) { offset.x = -5; } else { offset.x = offsetX; }
+        if (offsetY == 0) { offset.y = 5; } else { offset.y = offsetY; }
+
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        transform.position = player.transform.position + offset;
+        transform.LookAt(player.transform.position);
+	}
+}
---
+++ Assets/Scripts/PlayerManager.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+public class PlayerManager : MonoBehaviour {
+
+    public float attackDist;
+    
+    NavMeshAgent playerNav;
+
+    GameObject targetedEnemy;
+    bool isAttacking;
+
+	// Use this for initialization
+	void Start ()
+    {
+        playerNav = GetComponent<NavMeshAgent>();
+	}
+
+    void OnClick(Vector3 InputPos)
+    {
+        RaycastHit hit;
+        Ray ray = Camera.main.ScreenPointToRay(InputPos);
+
+        isAttacking = false;
+
+        if (Physics.Raycast(ray, out hit))
+        {
+            //If enemy clicked on
+            if(hit.transform.tag == "Enemy")
+            {
+                NewDestination(hit);
+                targetedEnemy = hit.transform.gameObject;
+                Debug.Log(targetedEnemy.name);
+            }
+            //If anything clicked on with no tag.
+            else
+            {
+                NewDestination(hit);
+                targetedEnemy = null;
+                if (playerNav.isStopped) { ToggleStop(); }
+            }
+        }
+    }
+
+
+    void ToggleStop()
+    {
+        playerNav.isStopped = !playerNav.isStopped;
+    }
+
+    void NewDestination(RaycastHit hitInfo)
+    {
+        playerNav.destination = hitInfo.point;
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		if(Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            OnClick(Input.mousePosition);
+        }
+
+        if(Input.touchCount > 0)
+        {
+            Vector3 pos = new Vector3(Input.touches[0].position.x, 0, Input.touches[0].position.y);
+            OnClick(pos);
+        }
+
+        if(targetedEnemy != null && !isAttacking)
+        {
+            float dist = Vector3.Distance(transform.position, targetedEnemy.transform.position);
+            if (dist < attackDist) { isAttacking = true; Debug.Log(isAttacking); ToggleStop(); }
+        }
+
+
+        //Test key
+        if(Input.GetKeyDown(KeyCode.E))
+        {
+            ToggleStop();
+        }
+	}
+}
